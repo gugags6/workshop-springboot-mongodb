75282fa8a68249e1757b2f5c8167c64aecfcd8c1
Gustavo Silva <guga.gs6@gmail.com>
2020-10-28 13:37:20 -0300

    Projeto criado

---
+++ src/test/java/com/cursospring/projetomongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.projetomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
